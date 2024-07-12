596b33f0bdae9cec80ddf7a139499773891b4045
Pulkit Agrawal <errorassassin101@gmail.com>
2024-07-12 17:56:08 +0530

    cookie settings

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_session import Session
 from .config import Config
 
 db = SQLAlchemy()
@@ -13,10 +14,16 @@ def create_app():
 
     app.config.from_object(Config)
     app.config['CORS_HEADERS'] = 'Content-Type'  # Set CORS headers
-
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+    app.config['SESSION_COOKIE_SECURE'] = True
+    
     db.init_app(app)
     migrate.init_app(app, db)
-
+    
+    # Initialize the session
+    Session(app)
+    
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
