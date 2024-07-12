404e29176b6c24530bd6978690fc96412a78aff9
Pulkit Agrawal <errorassassin101@gmail.com>
2024-07-12 17:42:27 +0530

    Revert "Update __init__.py"
    This reverts commit 481d71ae9b90f25c7c10db84e5c1504b13496cdb.

--- backend/flaskr/routes/__init__.py
+++ backend/flaskr/routes/__init__.py
@@ -9,8 +9,8 @@ from .customers import customers_bp
 from .products import products_bp
 from .transactions import transactions_bp
 
-main_bp.route('/api/')(lambda: "Welcome to the PoS System!")
-main_bp.register_blueprint(staff_bp, url_prefix='/api/staff')
-main_bp.register_blueprint(customers_bp, url_prefix='/api/customers')
-main_bp.register_blueprint(products_bp, url_prefix='/api/products')
-main_bp.register_blueprint(transactions_bp, url_prefix='/api/transactions')
+main_bp.route('/')(lambda: "Welcome to the PoS System!")
+main_bp.register_blueprint(staff_bp, url_prefix='/staff')
+main_bp.register_blueprint(customers_bp, url_prefix='/customers')
+main_bp.register_blueprint(products_bp, url_prefix='/products')
+main_bp.register_blueprint(transactions_bp, url_prefix='/transactions')
