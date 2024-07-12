026c3aa490f359e54d038ebff4728152e1a84687
Pulkit Agrawal <errorassassin101@gmail.com>
2024-07-12 14:11:36 +0530

    heroku

--- backend/run.py
+++ backend/run.py
@@ -2,6 +2,7 @@ from flaskr import create_app
 import os
 
 app = create_app()
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0', port=port, debug=True)
